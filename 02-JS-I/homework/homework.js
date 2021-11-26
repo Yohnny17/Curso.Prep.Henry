2ec4bdfeb7be10124abcb48f0cd4110c24324129
yohnny17 <yohnnyaospina@gmail.com>
2021-11-25 20:21:25 -0500

    area del triangulo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -223,19 +223,26 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+
+  var agregarSimboloExclamacion= str+"!";
+  return agregarSimboloExclamacion;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var combinarNombres = nombre+(" ")+apellido;
+  return combinarNombres;
+
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  var obtenerSaludo = "hola"+" "+nombre+"!";
+  return obtenerSaludo;
   
 }
 
@@ -243,6 +250,8 @@ function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
   
+  var obtenerAreaRectangulo = alto*ancho;
+  return obtenerAreaRectangulo;
 }
 
 
@@ -250,12 +259,17 @@ function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
   
+  var retornarPerimetro = lado* 4;
+  return retornarPerimetro;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+
+  var areaDelTriangulo = (base*altura)/2;
+  return areaDelTriangulo;
 
 }
 
@@ -264,7 +278,11 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var dolar= 1.20;
+  var euro= prompt("ingresa valor");
+var deEuroAdolar = (euro*dolar); 
+return deEuroAdolar;
+
 }
 
 
